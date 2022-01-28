d2ff63ca2e601ebc4a242d8fa8996ae6bb746930
elijahb22 <elibusick11@gmail.com>
2022-01-27 22:11:56 -0500

    created the function to initialize the app

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consolet = require('console.table');
+const db = require('./db/connection');
+
+// initalize the app
+db.connect(function (err) {
+    if (err) throw err;
+    console.log(`Employee Tracker`);
+    init();
+});
+
 
