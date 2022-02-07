f7ce79ea5c1895a2d95c90dec8f4061fd8998ff1
elijahb22 <elibusick11@gmail.com>
2022-02-06 22:28:47 -0500

    debugged

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,12 +7,12 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ('Sales Lead', $90000, 1),
-  ('Saleperson', $80000, 1),
-  ('Lead Engineer', $120000, 2),
-  ('Software Engineer', $100000, 2),
-  ('Lawyer', $150000, 3),
-  ('Accountant', $70000, 4);
+  ('Sales Lead', 90000, 1),
+  ('Saleperson', 80000, 1),
+  ('Lead Engineer', 120000, 2),
+  ('Software Engineer',$100000, 2),
+  ('Lawyer', 150000, 3),
+  ('Accountant', 70000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
